5eea2e75ba02cab40406b5d04b5e5fceb95d9399
etwem <etwemmett@gmail.com>
2026-05-16 00:28:32 +0100

    Small changes, removed/revoked API key from commands.txt.

--- session2/my_solutions/exercise-02-homework.py
+++ session2/my_solutions/exercise-02-homework.py
@@ -18,32 +18,25 @@ with open(DATA_DIR / "studio_ghibli_movies.csv", "r", encoding="utf-8", newline=
         #if row["year"] is None or row["year"].strip() == "":
         for column, value in row.items():
             if value is None or value.strip() == "":
-                missing_fields.append((row["movie_id"], row["title"], column))
+                missing_fields.append((i, row["title"], column))
                 missing_count += 1
 
 
 print(f"{missing_count} fields missing:\n{missing_fields}")
 
 for item in missing_fields:
-    line, title, field = item
-    try:
-        index = int(line.strip()) - 1
-    except ValueError:
-        print("Can't parse movie_id.")
+    index, title, field = item
 
     if field == "year":
         answer = ask_gemini(f"""Return only the 4-digit release year for the Studio Ghibli movie {title}.
 Output format: name only, no extra text.""")
-        rows_list[index]["year"] = answer
+        rows_list[index]["year"] = answer.strip()
 
     elif field == "music_by":
         answer = ask_gemini(f"""Return only the composer full name for the Studio Ghibli movie {title}.
 Output format: name only, no extra text.""")
-        rows_list[index]["music_by"] = answer
+        rows_list[index]["music_by"] = answer.strip()
     
-for row in rows_list:
-    print(row["year"])
-    print(row["music_by"])
 
 with open(DATA_DIR / "SG_movies_gemini_cleaned.csv", "w", encoding="utf-8", newline="") as file:
     writer = csv.DictWriter(file, fieldnames=header)
